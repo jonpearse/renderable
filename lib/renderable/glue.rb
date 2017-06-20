9bd244c59d9f5398105b4aba26b16996073d6585
Jon Pearse <jon@jonpearse.net>
2017-06-20 16:52:31 +0100

    Re-aligning whitespace against editorconfig
    It is safe to ignore this commit

--- lib/renderable/glue.rb
+++ lib/renderable/glue.rb
@@ -2,11 +2,11 @@ require 'renderable/schema'
 
 module Renderable
   module Glue
-    def self.included base      
+    def self.included base
       base.extend ClassMethods
       base.send :include, Callbacks
       base.send :include, Schema if defined? ActiveRecord
-      
+
       base.class_attribute :renderable_options
     end
   end
